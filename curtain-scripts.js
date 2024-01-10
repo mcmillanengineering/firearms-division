8eae6b5a5642ee9351d057e3c1e7f115626ae18e
mcmillanengineering <150833091+mcmillanengineering@users.noreply.github.com>
2024-01-10 13:32:13 +0000

    Create curtain-scripts.js

---
+++ curtain-scripts.js
@@ -0,0 +1,38 @@
+/* Open when someone clicks on the span element */
+	function openMenu() {
+	  document.getElementById("myMenu").style.height = "100%";
+	}
+	/* Close when someone clicks on the "x" symbol inside the overlay */
+	function closeMenu() {
+	  document.getElementById("myMenu").style.height = "0%";
+	}
+
+  function openShopMenu() {
+  document.getElementById("shopMenu").style.height = "100%";
+  }
+  function closeMenu2() {
+	document.getElementById("shopMenu").style.height = "0%";
+	}
+
+  function openServiceMenu() {
+  document.getElementById("serviceMenu").style.height = "100%";
+  }
+  function closeMenu3() {
+  document.getElementById("serviceMenu").style.height = "0%";
+  }
+
+  function openGunsmithingMenu() {
+  document.getElementById("gunsmithingMenu").style.height = "100%";
+  }
+  function closeMenu4() {
+  document.getElementById("gunsmithingMenu").style.height = "0%";
+  }
+
+  function openRifleMenu() {
+  document.getElementById("rifleMenu").style.height = 
+  "100%";
+  }
+  function closeMenu5() {
+  document.getElementById("rifleMenu").style.height = 
+  "0%";
+  }
